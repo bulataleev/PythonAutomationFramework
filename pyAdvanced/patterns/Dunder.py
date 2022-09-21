02dbe14312412c42fd009ed61602da62b2789afa
bulataleev <superbulat93@gmail.com>
2022-09-21 22:09:41 +0300

    call and repr dunder methods

--- pyAdvanced/patterns/Dunder.py
+++ pyAdvanced/patterns/Dunder.py
@@ -9,3 +9,22 @@ class Person:
 
 p = Person("Mike",25)
 del p
+
+class Vector:
+    def __init__(self,x,y):
+        self.x= x
+        self.y= y
+    def __add__(self,other):
+        return Vector(self.x + other.x,
+                    self.y + other.y)
+
+    def __repr__(self):
+        return f"X: {self.x}, Y is {self.y}"
+    
+    def __call__(self):
+        print("the object was called")
+
+v1 = Vector(1,2)
+v2 = Vector(1,2)
+v3= v1 + v2
+print(v3)
